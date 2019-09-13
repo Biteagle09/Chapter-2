f4fe0c500da6a1be0b85c5625b292a436cd78acc
Biteagle09 <2894544291@qq.com>
2019-09-13 09:22:20 +0800

    更改

--- Chapter-2.2/comm.h
+++ Chapter-2.2/comm.h
@@ -2,8 +2,8 @@
 
 #include "stdafx.h"
 
-//some public functiom
-extern void InitSqlist(sqList &sql);
+//some public function
+extern void InitSqlist(sqList& sql);
 extern void randMy(int arr[], int count, int limit);
 extern void InsertSqlistOfArray(sqList& sql, int n, int arr[]);
 extern void outputList(sqList L);
@@ -12,10 +12,10 @@ extern sqList increasingList(int count, int start);
 
 
 //algorithm function 
-extern int DeleteMin(sqList &L, int &min);
-extern void InverseList(sqList &L, int &result);
+extern int DeleteMin(sqList& L, int& min);
+extern void InverseList(sqList& L, int& result);
 extern int DeleteX(sqList& L, int x);
-extern int Delete_s_t(sqList &L, int s, int t);
+extern int Delete_s_t(sqList& L, int s, int t);
 extern int Delete_s_t_ooo(sqList& L, int s, int t);
 extern int DeleteRepatElem(sqList& L);
 extern int MergeList(sqList L1, sqList L2, sqList& L);
--- Chapter-2.2/stdafx.h
+++ Chapter-2.2/stdafx.h
@@ -37,5 +37,3 @@ typedef struct sqList {
 }sqList;
 
 
-
-
